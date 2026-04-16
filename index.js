b7d5fc7af227079d93b932ed20f628ae283f328c
newmorin2 <newton.mwangi@student.moringaschool.com>
2026-04-16 14:08:18 +0300

    created the display posts function

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your code here!
+function displayPosts(posts) {
+  const postList = document.getElementById("post-list");
+
+  postList.innerHTML = "";
+
+  posts.forEach(post => {
+    const li = document.createElement("li");
+
+    const h1 = document.createElement("h1");
+    h1.textContent = post.title;
+
+    const p = document.createElement("p");
+    p.textContent = post.body;
+
+    li.appendChild(h1);
+    li.appendChild(p);
+
+    postList.appendChild(li);
+  });
+}
